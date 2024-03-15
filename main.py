e0afcb98467145185e2686aec09b0e7181dfc351
Leon <leon.hansen@materna.group>
2024-03-15 15:10:06 +0100

    add react

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from openai import AzureOpenAI
 import os
+from flask import Flask
 
+app = Flask(__name__)
+
+
+@app.route("/")
+
+def index():
+    global pagetext
+    return pagetext
 
 def setup():
+    global pagetext
     client = AzureOpenAI(
         azure_endpoint=os.environ.get("OPENAI_BASE_URL"),
         api_key=os.environ.get("OPENAI_API_KEY"),
@@ -17,7 +27,7 @@ def setup():
         ],
         model="gpt-35-turbo",
     )
-
+    pagetext = response.choices[0].message
     print(response.choices[0].message)
 
 
@@ -25,3 +35,4 @@ def setup():
 
 if __name__ == "__main__":
     setup()
+    app.run(host="127.0.0.1", port=8080, debug=True)
